5612f32da04f99b11467baab784661e2ecd800f1
丁丁丁 <751850258@qq.com>
2021-08-14 01:43:09 +0800

    完善 js 操作

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 var box = document.querySelector('div');
 var h = document.querySelector('h1');
 var p = document.querySelector('p');
+let m = 1;
+
+p.onclick = function () {
+
+    switch (m % 3) {
+        case 2:
+            p.className = "";
+            p.className = 'three';
+            m++;
+            break;
+        case 1:
+            p.className = "";
+            p.className = 'tow';
+            m++;
+            break;
+        case 0:
+            p.className = "";
+            p.className = 'one';
+            m++;
+            break;
+    }
+}
